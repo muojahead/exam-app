927efdacaf04acb419a11ed4ac1130d7042eef6e
Mohamed Mujahid <mohamed.mojahead@gmail.com>
2021-10-23 00:13:49 +0200

    Enhancements on Question Answers Desgin

--- src/views/Question.vue
+++ src/views/Question.vue
@@ -12,6 +12,7 @@
               class="list-group-item"
               v-for="(answer, index) in allAnswersFuncResult"
               :key="index"
+              @click="getAnswer($event, index)"
             >
               <input
                 type="radio"
@@ -103,6 +104,9 @@ export default {
     },
   },
   methods: {
+    getAnswer(e, indexOfAnswer) {
+      this.selectedAnswer = this.allAnswersFuncResult[indexOfAnswer];
+    },
     allAnswers() {
       // Randomizing Array Elements to Change The correct Answer Position at every Question Showed
       const answers = [];
@@ -212,3 +216,61 @@ export default {
   },
 };
 </script>
+<style scoped lang="scss">
+.list-group-item {
+  transition: 0.2s ease-in all;
+  cursor: pointer;
+  &:hover {
+    color: #fff !important;
+    background-color: var(--bs-gray);
+  }
+}
+[type="radio"]:checked,
+[type="radio"]:not(:checked) {
+  position: absolute;
+  left: -9999px;
+}
+[type="radio"]:checked + label,
+[type="radio"]:not(:checked) + label {
+  position: relative;
+  padding-left: 28px;
+  cursor: pointer;
+  line-height: 20px;
+  display: inline-block;
+}
+[type="radio"]:checked + label:before,
+[type="radio"]:not(:checked) + label:before {
+  content: "";
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 18px;
+  height: 18px;
+  border: 1px solid #ddd;
+  border-radius: 100%;
+  background: #fff;
+}
+[type="radio"]:checked + label:after,
+[type="radio"]:not(:checked) + label:after {
+  content: "";
+  width: 12px;
+  height: 12px;
+  background: var(--bs-gray);
+  position: absolute;
+  top: 3.2px;
+  left: 3.2px;
+  border-radius: 100%;
+  -webkit-transition: all 0.2s ease;
+  transition: all 0.2s ease;
+}
+[type="radio"]:not(:checked) + label:after {
+  opacity: 0;
+  -webkit-transform: scale(0);
+  transform: scale(0);
+}
+[type="radio"]:checked + label:after {
+  opacity: 1;
+  -webkit-transform: scale(1);
+  transform: scale(1);
+}
+</style>
